d75e1296758b06975a978c0c56e7b13a2d9c8034
tahash <mtahash@gmail.com>
2020-08-21 12:53:04 +0530

    Updated settings

--- IMDB/settings.py
+++ IMDB/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -130,3 +130,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+django_heroku.settings(locals())
